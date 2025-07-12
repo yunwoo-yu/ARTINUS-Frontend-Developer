79252cb472334596c3d0cdf2f96b8e972deda462
Ycow <skypnal12@gmail.com>
2025-07-12 23:45:58 +0900

    feat: vite 경로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.PUBLIC_URL,
+  base: process.env.NODE_ENV === 'production' ? '/ARTINUS-Frontend-Developer/' : '/',
   plugins: [react(), tailwindcss()],
   build: {
     target: 'esnext',
