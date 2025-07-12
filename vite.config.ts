42272818b906c6bae752ba7007d7609ca0f98780
Ycow <skypnal12@gmail.com>
2025-07-12 23:50:30 +0900

    feat: router 처리

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/ARTINUS-Frontend-Developer/' : '/',
+  base: '/ARTINUS-Frontend-Developer/',
   plugins: [react(), tailwindcss()],
   build: {
     target: 'esnext',
